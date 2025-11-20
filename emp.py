0d092749a6585510f83e8c3d40b6fee16baee317
Rutu <kinnalrutu@gmail.com>
2025-11-20 15:09:58 +0530

    Create emp.py to display employee information
    Added a script to display employee details from command line arguments.

---
+++ emp.py
@@ -0,0 +1,14 @@
+import sys
+if len(sys.argv)!=5:
+  print("usage:python student.py <name> <roll>")
+  sys.exit(1)
+script_name=sys.argv[0]
+emp_name=sys.argv[1]
+emp_id=sys.argv[2]
+emp_sal=sys.argv[3]
+emp_exp=sys.argv[4]
+print("script name:",script_name)
+print("employee name:",emp_name)
+print("employee id:",emp_id)
+print("employee salary:",emp_name)
+print("employee experience:",emp_name)
