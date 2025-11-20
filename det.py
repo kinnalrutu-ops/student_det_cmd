0cc3f7223a7b5b1a7974d92a411aee4c25d7fb81
Rutu <kinnalrutu@gmail.com>
2025-11-20 14:03:57 +0530

    Add student information script in det.py

---
+++ det.py
@@ -0,0 +1,10 @@
+import sys
+if len(sys.argv)!=3:
+  print("usage:python student.py <name> <roll>")
+  sys.exit(1)
+script_name=sys.argv[0]
+name=sys.argv[1]
+roll=sys.argv[2]
+print("script name:",script_name)
+print("student name:",name)
+print("rollno:",roll)
